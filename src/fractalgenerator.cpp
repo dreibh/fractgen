49ef91b8a78de919bd68a309f3c9bce069281db6
Thomas Dreibholz <dreibh@simula.no>
2017-11-05 14:23:10 +0100

    Header updates.

--- src/fractalgenerator.cpp
+++ src/fractalgenerator.cpp
@@ -3,7 +3,7 @@
  * ====                   FRACTAL GRAPHICS GENERATOR                     ====
  * ==========================================================================
  *
- * Copyright (C) 2003-2017 by Thomas Dreibholz
+ * Copyright (C) 2003-2018 by Thomas Dreibholz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -245,7 +245,7 @@ void FractalGeneratorApp::slotHelpAbout()
    QMessageBox::information(this,
       tr("FractalGenerator II"),
       tr("FractalGenerator II\n") +
-         "Copyright (C) 2003-2017 by Thomas Dreibholz",
+         "Copyright (C) 2003-2018 by Thomas Dreibholz",
       tr("Okay"));
 }
 
