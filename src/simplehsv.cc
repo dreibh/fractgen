64c5d095b60b75e3d36bb074be8688a7a71f9b7d
Thomas Dreibholz <dreibh@simula.no>
2024-11-09 14:21:29 +0100

    Header updates.

--- src/simplehsv.cc
+++ src/simplehsv.cc
@@ -2,7 +2,7 @@
  * ====                   FRACTAL GRAPHICS GENERATOR                     ====
  * ==========================================================================
  *
- * Copyright (C) 2003-2024 by Thomas Dreibholz
+ * Copyright (C) 2003-2025 by Thomas Dreibholz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
